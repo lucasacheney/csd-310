64145379fdf25a93de9d71015c0a02ee4b466220
lacheney <lucasacheney@gmail.com>
2023-01-14 23:30:17 -0500

    Modification which includes code notation

--- module_5/mongdb_test.py
+++ module_5/mongdb_test.py
@@ -1,11 +1,23 @@
-from pymongo import MongoClient
-import certifi
+                    # Written By: Lucas Cheney
+                    # For Cyber410-T301 @ Bellevue University
+                    # Date: 1/14/2023
+                    # Attn: Professor Haas
 
+
+# MongDB Connection Module
+from pymongo import MongoClient
+# This was imported to deal with SSL certificate errors
+import certifi                  
+
+# Code for dealing with errors, the call is in line 17
 ca = certifi.where()
+
+# URL to MongoDB instance
 url = "mongodb+srv://admin:admin@cluster1.uksh1rj.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(url, tlsCAFile=ca)
 db = client.pytech
 
+# Main print function
 print("\n-- Pytech Collection List --")
 print(db.list_collection_names())
 print("\n\nEnd of program, press any key to exit...")
